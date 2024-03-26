ac3963bbd1991c5812c2345f79d2ffa31e6fbe67
Gr34sy <slupek3@poczta.fm>
2024-03-26 18:15:03 +0100

    added css reset and created navbar component using mui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+// components
+import Navbar from './components/Navbar';
+
+// styles
+import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,6 +20,24 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+
+      
+
+      <Navbar />
     </div>
   );
 }
