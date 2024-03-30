93d6d28244e0e756dc071df89f2930e5c64d894f
Gr34sy <slupek3@poczta.fm>
2024-03-30 14:20:55 +0100

    added icon to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,15 @@
-import '../styles/components/navbar.css';
+import "../styles/components/navbar.css";
 
-export function Navbar(){
-    return (
-        <nav className='navbar'>
-            User Dashboard
-        </nav>
-    )
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserTie } from "@fortawesome/free-solid-svg-icons";
+
+export function Navbar() {
+  return (
+    <nav className="navbar">
+      <header className="navbar__header">
+        <FontAwesomeIcon icon={faUserTie} className="navbar__icon" />
+        User Dashboard
+      </header>
+    </nav>
+  );
 }
